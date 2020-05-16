9e5b641b6027e8d110d9641fee35bd80a5e5503b
Jasmine-21 <jasmine171353.cse@chitkara.edu.in>
2020-05-16 11:49:58 +0530

    modified

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,23 +1,21 @@
-import React, {Component} from 'react';
-import {BrowserRouter as Router, Route } from 'react-router-dom';
-import {library} from '@fortawesome/fontawesome-svg-core';
-import {faIgloo} from '@fortawesome/free-solid-svg-icons';
-import Home from '../scrrens/Home/Home';
- library.add(faIgloo)
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faIgloo } from '@fortawesome/free-solid-svg-icons'
+import Home from '../screens/Home/Home';
+library.add(faIgloo)
 
- class AppRouter extends Component
- {
-     render()
-     {
-         return
-         (
-             <Router>
-                 <div className="main-controller">
-                    <Route exact path='/' render={(props)=><Home {...props} baseUrl={this.baseUrl} />} />
-                 </div>
-             </Router>
-         )
-     }
- }
+class AppRouter extends Component {
 
- export default AppRouter;
+  render() {
+    return (
+      <Router>
+        <div className="main-container">
+          <Route exact path='/' render={(props) => <Home {...props} baseUrl={this.baseUrl} />} />
+        </div>
+      </Router>
+    )
+  }
+}
+
+export default AppRouter;
